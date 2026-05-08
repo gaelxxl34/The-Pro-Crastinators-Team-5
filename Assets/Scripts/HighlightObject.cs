e04724901b2a51100942a6f679cebedbf38ab3b6
Rut1106 <rutpatel.34@gmail.com>
2026-05-08 14:17:38 -0500

    tweak: wired music player

--- Assets/Scripts/HighlightObject.cs
+++ Assets/Scripts/HighlightObject.cs
@@ -1,11 +1,41 @@
+// using UnityEngine;
+
+// public class HighlightObject : MonoBehaviour
+// {
+//     public Outline outline;
+
+    
+
+//     void Awake()
+//     {
+//         if (outline == null)
+//             outline = gameObject.AddComponent<Outline>();
+
+//         // outline.OutlineColor = Color.yellow;
+//         outline.OutlineWidth = 5f;
+//         outline.enabled = false;
+//     }
+
+//     public void Highlight()
+//     {
+//         outline.enabled = true;
+//     }
+
+//     public void Unhighlight()
+//     {
+//         outline.enabled = false;
+//     }
+// }
+
 using UnityEngine;
 
 public class HighlightObject : MonoBehaviour
 {
-    public Outline outline;
+    private Outline outline;
 
     void Awake()
     {
+        outline = GetComponent<Outline>();
         if (outline == null)
             outline = gameObject.AddComponent<Outline>();
 
